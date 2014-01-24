3d6c65f2221a0fb8e6837407e7deae283fc3d66d
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-01-24 15:26:02 -0600

    coding #1 adding elements to domain class notification

--- grails-app/domain/com/makingdevs/Notification.groovy
+++ grails-app/domain/com/makingdevs/Notification.groovy
@@ -2,6 +2,17 @@ package com.makingdevs
 
 class Notification {
 
+    String to
+    String from
+    String subject
+    Template template
+
+    Date dateCreted
+    Date lastUpdated  
+
     static constraints = {
+      to nullable:false,blank:false
+      from blank:false, nullable:false
+      subject nullable:false      
     }
 }
