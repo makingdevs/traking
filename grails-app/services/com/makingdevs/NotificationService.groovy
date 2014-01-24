5b07d4abdd6e6d0a0a8f7f3179c81e7cfbd8c14a
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-01-24 16:45:37 -0600

    coding #2 inyect mailservice and add method for send mail

--- grails-app/services/com/makingdevs/NotificationService.groovy
+++ grails-app/services/com/makingdevs/NotificationService.groovy
@@ -5,7 +5,14 @@ import grails.transaction.Transactional
 @Transactional
 class NotificationService {
 
-    def serviceMethod() {
+    def mailService
 
+    def sendNotificationTo(Notification notification) {
+      mailService.sendMail {
+        from notification.from
+        bcc notification.to
+        subject notification.subject
+        html notification.template
+      }
     }
 }
