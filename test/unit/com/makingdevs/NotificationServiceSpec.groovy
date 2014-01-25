2d553f630f4d1157f44b81f4bcbcd9d8c99a0eff
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-01-24 18:00:39 -0600

    cosing #2 begin created first test for notificationService

--- test/unit/com/makingdevs/NotificationServiceSpec.groovy
+++ test/unit/com/makingdevs/NotificationServiceSpec.groovy
@@ -1,20 +1,31 @@
 package com.makingdevs
 
-import grails.test.mixin.TestFor
+import static org.junit.Assert.*
+import grails.test.mixin.*
+import org.unit.*
 import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(NotificationService)
+@Mock(Notification)
 class NotificationServiceSpec extends Specification {
 
-	def setup() {
-	}
+    def "Created a object Notification and invoke the method sendNotificationTo"() {
+      given:
+       def templates = new Template()
+       templates.body "super prueba" 
+       templates.save(validate:false)
+      and:
+        def notification = new Notification()
+        notification.to = ['sergio@makingdevs.com', 'says.rodriguez@gmail.com'] as String[]
+        notification.from = "radamantyz_033@hotmail.com"
+        notification.subject = "super duper prueba"
+        notification.template = templates
+        notification.save(validate:false)      
+      
+    }
 
-	def cleanup() {
-	}
 
-	void "test something"() {
-	}
 }
