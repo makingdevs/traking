65c5299a03bec7ea2297e48108ab593fd7854fb5
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-02-06 09:52:27 -0600

    coding #7 create service for the creation of sending mails log

--- grails-app/services/com/makingdevs/RecordLogService.groovy
+++ grails-app/services/com/makingdevs/RecordLogService.groovy
@@ -2,7 +2,7 @@ package com.makingdevs
 
 class RecordLogService {
 
-    def createRecorLogAndSave(Notification notificationPerInstance) {
+    def createRecordLogAndSave(Notification notificationPerInstance) {
       def log = new RecordLog(notification:notificationPerInstance).save(flush:true)
       log
     }
