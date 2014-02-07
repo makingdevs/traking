c0a0603d003f7db1eaf81d2e52cebf055cf3496d
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-02-07 13:47:12 -0600

    coding #7 create test for trackingService

--- test/unit/com/makingdevs/TrackingServiceSpec.groovy
+++ test/unit/com/makingdevs/TrackingServiceSpec.groovy
@@ -6,17 +6,39 @@ import static org.junit.Assert.*
 import org.unit.*
 
 @TestFor(TrackingService)
-@Mock(MailStructureCommand)
+@Mock([RecordLog,Notification])
 class TrackingServiceSpec extends Specification {
 
 
   def "create a notification in base to command"() {
     given:
       def msc = new MailStructureCommand()
-      
+      msc.to = "s@s.com"
+      msc.sender = "d@d.com"
+      msc.subject = "susto"
+      msc.body = "ohffojnndfnj"
+    and:
+      def mocks = createPartners()
     when:
+      def result = service.trackingNotificationBy(msc) 
+      mocks*.verify()
     then:
-    where:
+      assert result instanceof RecordLog
   }
 
+
+  private def createPartners() {
+    Notification notification = new Notification()
+    RecordLog recordLog = new RecordLog()
+    def notificationServiceMock = mockFor(NotificationService)
+    notificationServiceMock.demand.createNewMailStructure(1..1){ obj -> return notification}
+    notificationServiceMock.demand.sendNotificationTo(1..1){ obje -> return true}
+    service.notificationService  = notificationServiceMock.createMock()
+    
+    def recordLogServiceMock = mockFor(RecordLogService)
+    recordLogServiceMock.demand.createRecordLogAndSave(1..1){ objec -> return recordLog}
+    service.recordLogService = recordLogServiceMock.createMock()
+    [notificationServiceMock, recordLogServiceMock] 
+  }
+  
 }
