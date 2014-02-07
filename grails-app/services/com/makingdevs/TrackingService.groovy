fa4ab6f6c2fa87e18cb0acdecf8abfdfb0dc6ade
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-02-07 13:45:38 -0600

    coding #7 modify name of service recodlog and return of service trackingNotificationBy

--- grails-app/services/com/makingdevs/TrackingService.groovy
+++ grails-app/services/com/makingdevs/TrackingService.groovy
@@ -3,13 +3,13 @@ package com.makingdevs
 class TrackingService {
 
     def notificationService
-    def recordLongService
+    def recordLogService
 
     def trackingNotificationBy(MailStructureCommand msc) {
       def notification = notificationService.createNewMailStructure(msc)
       def responseOfSendMail = notificationService.sendNotificationTo(notification)
       if (responseOfSendMail) {
-        def responseOfLog = recordLongService recordLongService.createRecordLogAndSave(notification) 
+        return recordLogService.createRecordLogAndSave(notification) 
       }
     }
 }
