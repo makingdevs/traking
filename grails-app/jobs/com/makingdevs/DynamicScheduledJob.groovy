6b27cd3539cea91705007876d346a71acaa83eb3
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-02-14 11:48:06 -0600

    coding #8 create a Dynamic Job

---
+++ grails-app/jobs/com/makingdevs/DynamicScheduledJob.groovy
@@ -0,0 +1,13 @@
+package com.makingdevs
+
+
+
+class DynamicScheduledJob {
+    static triggers = {
+      simple repeatInterval: 5000l // execute job once in 5 seconds
+    }
+
+    def execute() {
+        // execute job
+    }
+}
