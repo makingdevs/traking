35eefa6d4ca748cd52fe851e4d1385709ff74bac
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-02-14 13:59:56 -0600

    coding #8 create ,methods for create a dynamic job

--- grails-app/services/com/makingdevs/DynamicScheduledService.groovy
+++ grails-app/services/com/makingdevs/DynamicScheduledService.groovy
@@ -2,7 +2,22 @@ package com.makingdevs
 
 class DynamicScheduledService {
 
-    def serviceMethod() {
-
+    def createdScheduledTask(def paramsRequest) {
+      ScheduledTask task = new ScheduledTask()
+      task.schedulerTrigger = paramsRequest.schedulerTrigger
+      task.schedulerExpression = getExpression(params.schedulerTrigger).toString()
+      task.statusScheduled = StatusScheduled.REGISTERED
+      task.notification = Notification.get(paramsRequest.idNotification) 
     }
+
+    private def getExpression(Date scheduledDate) {
+      def date = new Date(scheduledDate.toString())
+      def year = date.getAt(Calendar.YEAR)
+      def month = date.getAt(Calenar.MONTH) + 1
+      def day = date.getAt(Calendar.DAY_OF_MONTH)
+      def hour = date.getAt(Calendar.HOUR)
+      def minute = date.getAt(Calendar.MINUTE)
+      return "* "+minute+" "+hour+" "+day+" "+month+" "+year
+    }    
+
 }
